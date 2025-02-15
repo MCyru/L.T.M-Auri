75aa43a992fea273350dca3b2b6b8caa61117d5a
MCyru <kevinclaytoncordeirosantos@gmail.com>
2025-02-15 18:32:12 -0300

    Added search to the database.js

--- database.js
+++ database.js
@@ -13,16 +13,39 @@ function writeDatabase(filename, data) {
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
 
-function queryDatabase(filename, query) {
-    const data = readDatabase(filename);
-    return data.filter(query);
+function searchDatabase(query) {
+    const files = fs.readdirSync(dataFolderPath);
+    const results = [];
+
+    files.forEach(file => {
+        const data = readDatabase(file);
+        if (typeof data === 'object') {
+            searchInObject(data, query, results, file);
+        }
+    });
+
+    return results;
+}
+
+function searchInObject(obj, query, results, filePath, currentPath = '') {
+    for (const key in obj) {
+        const value = obj[key];
+        const newPath = currentPath ? `${currentPath}.${key}` : key;
+
+        if (typeof value === 'object') {
+            searchInObject(value, query, results, filePath, newPath);
+        } else if (String(value).includes(query)) {
+            results.push({ filePath, path: newPath, value });
+        }
+    }
 }
 
 
 module.exports = {
     readDatabase,
     writeDatabase,
-    queryDatabase
+    searchDatabase,
+    searchInObject
 };
 
 //const { readDatabase, writeDatabase } = require('../database');
